573b303b9134fde03120369678ac994fdfb5d912
hugh <hugh@hughs-MacBook-Air.local>
2025-03-19 16:06:58 +0100

    restructure files

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# thesis
-# thesis
+# How to run this app?
+Be under the top-level dir of this repo: 
+> sh run.sh
--- src/app.py
+++ src/app.py
@@ -26,24 +26,6 @@ model = TFAutoModelForSeq2SeqLM.from_pretrained('../liedetectionllm')
 
 with open("./style/style.css") as f:
     st.markdown("<style>{}</style>".format(f.read()), unsafe_allow_html=True)
-#from google.oauth2.service_account import Credentials
-#from streamlit.components.v1 import html
-
-# Google Sheets authentication
-#scope = ["https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive"]
-#creds = Credentials.from_service_account_info(st.secrets["google_service_account"], scopes=scope)
-#client = gspread.authorize(creds)
-
-# Open your Google Sheet (replace "Sheet_Name" with your actual sheet name)
-#sheet_1 = client.open("Database").sheet1  # Access the first sheet
-#sheet_2 = client.open("Database").get_worksheet(1)  # Access the second sheet
-
-# Function to submit data to Google Sheets
-#def submit_to_sheet_1(data):
-#    sheet_1.append_row(data)
-
-#def submit_to_sheet_2(data):
-#    sheet_2.append_row(data)
 
 # Define progress bar
 total_steps = 22
@@ -52,7 +34,6 @@ total_steps = 22
 # Page Navigation Logic
 if 'page' not in st.session_state:
     st.session_state.page = 'welcome'
-
 if st.session_state.page == 'welcome':
     pages.welcome_page()
 elif st.session_state.page == 'consent':
